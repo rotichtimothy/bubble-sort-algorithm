4966b0360c6e512b007508d41dd5842c0b76f3a4
Timothy Rotich <111359150+rotichtimothy@users.noreply.github.com>
2024-07-13 14:36:46 +0300

    Add files via upload

---
+++ bubble-sort-algorithm.py
@@ -0,0 +1,13 @@
+# Bubble Sort
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+array = [64, 34, 25, 12, 22, 11, 90]
+sorted_array = bubble_sort(array)
+print("Sorted array is:", sorted_array)
